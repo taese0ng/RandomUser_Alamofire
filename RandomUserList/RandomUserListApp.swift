cb60398b2a680aff45ba288e612c2d9df6a74777
taese0ng <taese0ng@naver.com>
2022-02-05 23:20:43 +0900

    Initial Commit

---
+++ RandomUserList/RandomUserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserListApp.swift
+//  RandomUserList
+//
+//  Created by taese0ng on 2022/02/05.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
